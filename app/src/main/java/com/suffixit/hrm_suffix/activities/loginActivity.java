87d11e8cfe29cb3f78e364e05f54fe0be588c957
Nahid Hasan <45915292+nahid1126@users.noreply.github.com>
2023-09-07 11:43:55 +0600

    init

---
+++ app/src/main/java/com/suffixit/hrm_suffix/activities/loginActivity.java
@@ -0,0 +1,16 @@
+package com.suffixit.hrm_suffix.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.suffixit.hrm_suffix.R;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
