cdf5522752f211e8f89ff3d604935e994dc45704
sohel2 <sohel.hafizurrahman@gmail.com>
2023-09-10 19:06:19 +0600

    login page design

--- app/src/main/java/com/suffixit/hrm_suffix/activities/loginActivity.java
+++ app/src/main/java/com/suffixit/hrm_suffix/activities/loginActivity.java
@@ -2,16 +2,44 @@ package com.suffixit.hrm_suffix.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.suffixit.hrm_suffix.R;
 
 public class loginActivity extends AppCompatActivity {
 
+    TextView loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton=findViewById(R.id.loginbtn);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            );
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(loginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
