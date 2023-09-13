89e76a02f0a7916adcfef1dce65bc04eb238aaa0
sohel2 <sohel.hafizurrahman@gmail.com>
2023-09-13 18:44:35 +0600

    employee list design

--- app/src/main/java/com/suffixit/hrm_suffix/view/Activities/MainActivity.java
+++ app/src/main/java/com/suffixit/hrm_suffix/view/Activities/MainActivity.java
@@ -1,16 +1,34 @@
 package com.suffixit.hrm_suffix.view.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
-
+import android.view.View;
 import com.suffixit.hrm_suffix.R;
+import com.suffixit.hrm_suffix.databinding.ActivityMainBinding;
+import com.suffixit.hrm_suffix.view.Fragment.EmployeeFragment;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.imgEmplyee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment fragment=new EmployeeFragment();
+
+                FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+              //  fragmentTransaction.replace(R.id.continer, fragment).commit();
+
+            }
+        });
     }
 }
