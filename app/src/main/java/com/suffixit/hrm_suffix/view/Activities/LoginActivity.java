9e0a6ed659321959869e2c672730ced2ba4a2f38
sohel2 <sohel.hafizurrahman@gmail.com>
2023-09-14 12:08:18 +0600

    login with username

--- app/src/main/java/com/suffixit/hrm_suffix/view/Activities/LoginActivity.java
+++ app/src/main/java/com/suffixit/hrm_suffix/view/Activities/LoginActivity.java
@@ -1,33 +1,48 @@
+
 package com.suffixit.hrm_suffix.view.Activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.suffixit.hrm_suffix.R;
 import com.suffixit.hrm_suffix.databinding.ActivityLoginBinding;
+import com.suffixit.hrm_suffix.utils.KeyboardUtils;
+import com.suffixit.hrm_suffix.utils.NetworkUtils;
+import java.util.Map;
 
 
 public class LoginActivity extends AppCompatActivity {
     ActivityLoginBinding binding;
     private FirebaseAuth auth;
+    private DatabaseReference usersRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
         auth = FirebaseAuth.getInstance();
+        usersRef = FirebaseDatabase.getInstance().getReference("users");
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -37,45 +52,61 @@ public class LoginActivity extends AppCompatActivity {
             );
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
-
+        if (!NetworkUtils.isNetworkConnected(LoginActivity.this)) {
+            KeyboardUtils.hideKeyboard(LoginActivity.this);
+            Toast.makeText(this, R.string.no_internet, Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginBtn(v);
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                if (currentUser != null) {
+                    String username = binding.userName.getText().toString();
+                    String password = binding.password.getText().toString();
+                    authenticateUser(username, password);
+                } else {
+                    Toast.makeText(LoginActivity.this, "User is not signed in.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
-    public void loginBtn(View view){
-        String userEmail= binding.userName.getText().toString().trim();
-        String userPassword= binding.password.getText().toString().trim();
-
-        if (TextUtils.isEmpty(userEmail)){
-            Toast.makeText(this, "Enter the Email!", Toast.LENGTH_SHORT).show();
+    private void authenticateUser(final String username, final String enteredPassword) {
+        if (!NetworkUtils.isNetworkConnected(LoginActivity.this)) {
+            KeyboardUtils.hideKeyboard(LoginActivity.this);
+            Toast.makeText(LoginActivity.this, R.string.no_internet, Toast.LENGTH_SHORT).show();
             return;
         }
 
-        if (TextUtils.isEmpty(userPassword)){
-            Toast.makeText(this, "Enter the Password!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if (userPassword.length()<6){
-            Toast.makeText(this, "Password too short!, Enter minimum 4 characters!", Toast.LENGTH_SHORT).show();
-            return;
-        }
+        CollectionReference usersCollection = FirebaseFirestore.getInstance().collection("Users");
+        Query query = usersCollection.whereEqualTo("username", username);
 
-        auth.signInWithEmailAndPassword(userEmail, userPassword)
-                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    boolean userFound = false;
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Map<String, Object> userData = document.getData();
+                        String storedPassword = (String) userData.get("password");
 
-                        if (task.isSuccessful()){
-                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        if (storedPassword != null && storedPassword.equals(enteredPassword)) {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
                             finish();
-                        } else {
-                            Toast.makeText(LoginActivity.this, "Bad Credential", Toast.LENGTH_SHORT).show();
+                            userFound = true;
+                            break;
                         }
                     }
-                });
-        }
+                    if (!userFound) {
+                        Toast.makeText(LoginActivity.this, R.string.bad_credential, Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this, "Error querying Firestore: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+            }
+        });
+    }
 }
