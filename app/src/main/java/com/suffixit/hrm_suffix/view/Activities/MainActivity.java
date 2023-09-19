a15cd98fdcf8cfd670b1a353aad44c982ade865c
Hafizur Rahman <sohel.hafizurrahman@gmail.com>
2023-09-19 19:40:43 +0600

    find out login user not create yet

--- app/src/main/java/com/suffixit/hrm_suffix/view/Activities/MainActivity.java
+++ app/src/main/java/com/suffixit/hrm_suffix/view/Activities/MainActivity.java
@@ -1,14 +1,12 @@
 package com.suffixit.hrm_suffix.view.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 import android.view.View;
 import com.suffixit.hrm_suffix.R;
 import com.suffixit.hrm_suffix.databinding.ActivityMainBinding;
-import com.suffixit.hrm_suffix.view.Fragment.EmployeeFragment;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
@@ -19,27 +17,20 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.imgEmplyee.setOnClickListener(new View.OnClickListener() {
+      /*  binding.cardViewEmplyee.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, EmployeeFragment.class);
+           // intent.putExtra(AppConstants.LOAD_FRAGMENT, AppConstants.FLAT_FRAGMENT);
+            startActivity(intent);*/
+
+
+        binding.cardViewEmplyee.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                navigateToEmployeeFragment();
-
+             //  FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+            //   fragmentTransaction.replace(R.id.fragment_continer, new BlankFragment()).commit();
             }
         });
+
     }
+}
 
-        private void navigateToEmployeeFragment() {
-            // Create a new instance of the EmployeeFragment
-            EmployeeFragment employeeFragment = new EmployeeFragment();
-
-            // Get the fragment manager
-            FragmentManager fragmentManager = getSupportFragmentManager();
-
-            // Start a fragment transaction to replace the current fragment with the EmployeeFragment
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-            transaction.replace(R.id.fragment_container, employeeFragment); // Replace with your fragment container ID
-            transaction.addToBackStack(null); // Optional: Adds the transaction to the back stack
-            transaction.commit();
-        }
-    }
